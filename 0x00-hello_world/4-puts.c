b917070477156253bc18430c2f732f022b601d8b
Muhammed-Sameih <muhamedsamih748@gmail.com>
2023-03-16 06:22:36 -0700

    update 4-puts file

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 /*
- * The entry point : mian function that return 0
+ * main - Entry point
+ * 
+ * Return: 0
+ *
  * */
 int main(void)
 {
-
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
