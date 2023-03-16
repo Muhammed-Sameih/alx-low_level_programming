57f712f31acf54612f08a5bab7a86416d4ea9c7e
Muhammed-Sameih <muhamedsamih748@gmail.com>
2023-03-16 06:11:09 -0700

    create 4-puts c file

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
+/*
+ * The entry point : mian function that return 0
+ * */
+int main(void)
+{
 
-int main(void){
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
